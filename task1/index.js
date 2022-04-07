8fe7394f0ba13d0612bd7e0b6c79e508c5302730
Lingsbay <predchishindmytro@gmail.com>
2022-04-07 18:03:30 +0300

    "Task #1 solution"

--- task1/index.js
+++ task1/index.js
@@ -1,3 +1,17 @@
 function calculate(str) {
+    // Variable for containing result
+    let resultString = '';
 
+    const arr = str.split(' ');
+    let [num1, operator, num2] = [arr[0].length, arr[1], arr[2].length];
+
+    if (operator === '+') {
+        return resultString.padStart(num1 + num2, '.');
+    } else if (operator === '-') {
+        return resultString.padStart(num1 - num2, '.');
+    } else if (operator === '*') {
+        return resultString.padStart(num1 * num2, '.');
+    } else (operator === '//'); {
+        return resultString.padStart(Math.floor(num1 / num2), '.');
+    }
 }

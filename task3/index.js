d697a3d2fd1727f4d81aa636e286660564b50208
Lingsbay <predchishindmytro@gmail.com>
2022-04-07 18:06:22 +0300

    "Task #3 solution"

--- task3/index.js
+++ task3/index.js
@@ -1,3 +1,14 @@
 function isValidParentheses(str) {
+    const stack = [];
 
+    for(let i = 0; i < str.length; i++) {
+        if (str[i] === '(') {
+            stack.push(str[i]);
+        } else if (str[i] === ')' && stack[stack.length - 1] === '(') {
+            stack.pop();
+        } else {
+            return false;
+        }
+    }
+    return stack.length === 0;
 }

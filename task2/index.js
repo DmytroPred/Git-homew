14325debb7216221c0d7453a7e7ca940083d4b77
Lingsbay <predchishindmytro@gmail.com>
2022-04-07 18:04:59 +0300

    "Task #2 solution"

--- task2/index.js
+++ task2/index.js
@@ -1,3 +1,12 @@
 function grid(N) {
-
+    if (N < 0) return null;
+    const rowArr = [];
+    for (let row = 0; row < N; row++) {
+        const charArr = [];
+        for (let i = 0; i < N; i++) {
+            charArr.push(String.fromCharCode('a'.charCodeAt() + (row + i) % 26));
+        }
+        rowArr.push(charArr.join(' '));
+    }
+  return rowArr.join('\n');
 }
